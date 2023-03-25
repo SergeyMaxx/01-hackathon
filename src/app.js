8950dbb95c132e897831890cd9010afd1f11b105
Aleksandr Malnev <asmalnev@gmail.com>
2023-03-25 23:06:11 +0300

    модуль всплывающего сообщения

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
-import './styles.css'
+import './styles.css';
+import { Message } from './modules/message.module';
 
+const myMessage = new Message('message', 'Вызвать сообщение');
+
+// для тестирования всплывающего сообщения
+const messageButton = document.createElement('button');
+messageButton.textContent = 'Показать сообщение';
+messageButton.addEventListener('click', () => {
+  myMessage.trigger('Заголовок сообщения', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit');
+});
+document.body.append(messageButton);
