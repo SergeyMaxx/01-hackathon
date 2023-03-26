f30c38b6283856fe557563c1e4b3b14eda72e5fe
Kirill-N-dev <123206565+Kirill-N-dev@users.noreply.github.com>
2023-03-26 12:53:45 +0200

    Update jingle.module.js
    Deleted "#"

--- src/modules/jingle.module.js
+++ src/modules/jingle.module.js
@@ -1,7 +1,7 @@
 import { Module } from '../core/module';
 
 export class JingleModule extends Module {
-  #trigger() {
+  trigger() {
     var audio = new Audio();
     audio.src = '../media/mario_coin_sound.mp3';
     audio.autoplay = true;
