3b4c96ded5c4d3c84948e456ff6a3cd72ab219d2
Aleksandr Malnev <asmalnev@gmail.com>
2023-03-26 16:42:31 +0300

    app.js очищен, доработан модуль сообщений

--- src/app.js
+++ src/app.js
@@ -1,12 +1,1 @@
 import './styles.css';
-import { Message } from './modules/message.module';
-
-const myMessage = new Message('message', 'Вызвать сообщение');
-
-// для тестирования всплывающего сообщения
-const messageButton = document.createElement('button');
-messageButton.textContent = 'Показать сообщение';
-messageButton.addEventListener('click', () => {
-  myMessage.trigger('Заголовок сообщения', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit');
-});
-document.body.append(messageButton);
--- src/modules/message.module.js
+++ src/modules/message.module.js
@@ -1,10 +1,12 @@
 import { Module } from '../core/module';
+import { random } from '../utils';
 import './message.css';
 
 export class Message extends Module {
   #hideTimeout;
   #showTimeout;
   #removeTimeout;
+  #messageArrays;
 
   constructor(type, text) {
     super(type, text);
@@ -13,7 +15,37 @@ export class Message extends Module {
     this.#showTimeout = 150; // тайминг из css на показ
     this.#removeTimeout = 300; // тайминг из css на скрытие
 
-    this.timeoutID = undefined;
+    // массив сообщений
+    this.#messageArrays = [
+      {
+        title: 'Противоположная точка зрения подразумевает',
+        text: 'что ключевые особенности структуры проекта, превозмогая сложившуюся непростую экономическую ситуацию, описаны максимально подробно'
+      },
+      {
+        title: 'Господа, начало повседневной работы',
+        text: 'по формированию позиции не оставляет шанса для первоочередных требований'
+      },
+      {
+        title: 'Безусловно, современная методология разработки',
+        text: 'требует определения и уточнения вывода текущих активов!'
+      },
+      {
+        title: 'Банальные, но неопровержимые выводы',
+        text: 'а также представители современных социальных резервов освещают чрезвычайно интересные особенности картины в целом.'
+      },
+      {
+        title: 'Однако конкретные выводы',
+        text: 'разумеется, преданы социально-демократической анафеме!'
+      },
+      {
+        title: 'Учитывая ключевые сценарии поведения',
+        text: 'понимание сути ресурсосберегающих технологий обеспечивает широкому кругу (специалистов) участие в формировании укрепления моральных ценностей.'
+      },
+      {
+        title: 'А также активно развивающиеся страны третьего мира',
+        text: 'освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, ассоциативно распределены по отраслям.'
+      }
+    ];
   }
 
   // приватный метод создания контейнера сообщений
@@ -25,31 +57,31 @@ export class Message extends Module {
   }
 
   // приватный метод создания сообщения
-  #createMessage(messageTitle, messageText) {
+  #createMessage({ title, text }) {
     // само сообщение
     const message = document.createElement('div');
     message.className = 'message';
 
     // заголовок
-    const title = document.createElement('div');
-    title.className = 'message__title';
-    title.textContent = messageTitle;
+    const messageTitle = document.createElement('div');
+    messageTitle.className = 'message__title';
+    messageTitle.textContent = title;
 
     // текст
-    const text = document.createElement('div');
-    text.className = 'message__text';
-    text.textContent = messageText;
+    const messageText = document.createElement('div');
+    messageText.className = 'message__text';
+    messageText.textContent = text;
 
-    message.append(title, text);
+    message.append(messageTitle, messageText);
 
     // делегирование клика на сообщение
     document.addEventListener('click', (event) => {
       const
         { target } = event,
-        message = target.closest('.message');
+        messageItem = target.closest('.message');
 
-      if (message) {
-        this.#removeMessage(message); // удаление сообщения
+      if (messageItem) {
+        this.#removeMessage(messageItem); // удаление сообщения
       }
     });
 
@@ -70,7 +102,7 @@ export class Message extends Module {
   #hideMessage(message) {
     setTimeout(() => {
       message.classList.remove('active');
-      this.#removeMessage(message);
+      this.#removeMessage(message, true);
     }, this.#hideTimeout);
   }
 
@@ -87,8 +119,10 @@ export class Message extends Module {
 
   // приватный метод добавления контейнера сообщений
   #addContainer() {
+    // ищем контейнер на странице
     let container = document.querySelector('.messages-container');
 
+    // если его нет - создаем
     if (!container) {
       container = this.#createContainer();
     }
@@ -97,15 +131,22 @@ export class Message extends Module {
   }
 
   // триггер по ТЗ
-  trigger(title, text) {
+  trigger() {
     const
       container = this.#addContainer(), // получаем контейнер
-      message = this.#createMessage(title, text); // получаем сообщение
 
-    container.append(message); // добавляем сообщение в контейнер
+      // получаем случайно выбранное сообщение из массива
+      messageFromArray = this.#messageArrays[random(0, this.#messageArrays.length - 1)],
 
-    document.body.append(container); // добавляем контейнер в body
+      message = this.#createMessage(messageFromArray); // получаем сообщение
 
-    this.#showMessage(message); // показываем сообщение
+    // добавляем сообщение в контейнер
+    container.append(message);
+
+    // добавляем контейнер в body
+    document.body.append(container);
+
+    // показываем сообщение
+    this.#showMessage(message);
   }
 }
